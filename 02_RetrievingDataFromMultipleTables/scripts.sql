2e4d9a505aa7fa50fb160344b92dc4fdd9198542
Atul Prakash <berealatul@gmail.com>
2025-04-04 14:11:30 +0530

    SELF JOIN, JOIN By USING clause, NATURAL JOIN, CROSS Join, UNION

--- 02_RetrievingDataFromMultipleTables/scripts.sql
+++ 02_RetrievingDataFromMultipleTables/scripts.sql
@@ -2,6 +2,7 @@
     -- LINUX: system rm -f output.txt; tee output.txt; source scripts.sql; notee;
     -- WINDOWS: system del -f output.txt; tee output.txt; source scripts.sql; notee;
     -- it will first remove output.txt file and then initialize it to record result of scripts.sql
+    -- always put space after "-- " while commenting otherwise MySQL will yell at you
     
 
 -- Retrieving Data From Multiple Tables
@@ -59,9 +60,8 @@
     -- because it is reflecting price at which it was purchased in that order
 
 
-
 -- JOINING ACROSS DATABASES
-SHOW DATABASES;
+    SHOW DATABASES;
 
     -- join sql_store databases order item table with sql_inventory products table
     -- since we are using sql_store database so for sql_inventory database 
@@ -140,6 +140,7 @@ SHOW DATABASES;
             ON oi.order_id = oin.order_id
             AND oi.product_id = oin.product_id;
 
+
 -- IMPLICIT JOIN SYNTAX
     -- i.e. using where clause
     SELECT * 
@@ -151,7 +152,6 @@ SHOW DATABASES;
     SELECT *
     FROM orders, customers;
 
- 
 
 -- OUTER JOIN
     -- are of two types LEFT JOIN & RIGHT JOIN
@@ -213,7 +213,7 @@ SHOW DATABASES;
     LEFT JOIN order_items oi
         ON p.product_id = oi.product_id;
 
-system cls;
+
 -- OUTER JOIN BETWEEN MULTIPLE TABLES
     SHOW tables;
     DESCRIBE orders;
@@ -238,7 +238,7 @@ system cls;
     DESCRIBE shippers;
     DESCRIBE order_statuses;
 
-    system cls;
+    
     SELECT
         o.order_date,
         o.order_id,
@@ -256,3 +256,195 @@ system cls;
 
     -- note that database column name are sometime same for different column while sometime it
     -- is different although refering to same thing because in relaity it like that only
+
+
+-- SELF OUTER JOIN
+    -- retrive employees with their managers from employees table in sql_hr
+    USE sql_hr;
+    SHOW tables;
+    DESCRIBE employees;
+
+    SELECT 
+        e.employee_id, 
+        e.first_name,
+        m.first_name AS manager
+    FROM employees e
+    LEFT JOIN employees m
+        ON e.reports_to = m.employee_id;
+
+
+-- JOIN BY USING CLAUSE (can be used for both INNER & OUTER JOIN)
+    -- if the attributes of the table on which we are joining
+    -- are of the same title then we can take advantage of USING clause
+    USE sql_store;
+    DESCRIBE orders;
+    DESCRIBE shippers;
+
+    -- extract order id, customer name and shipper name for every order 
+    SELECT
+        o.order_id,
+        c.first_name,
+        s.name AS shipper
+    FROM orders o
+    LEFT JOIN customers c
+        -- ON o.customer_id = c.customer_id
+        USING (customer_id)
+    LEFT JOIN shippers s
+        USING (shipper_id);
+
+    -- if join is to be done based on multiple attributes
+    -- use USING(col1, col2, ...) int this format
+    
+    DESCRIBE order_items;
+    DESCRIBE order_item_notes;
+
+    SELECT 
+        oi.order_id,
+        oi.product_id,
+        oi.quantity,
+        oi.unit_price,
+        oin.note
+    FROM order_items oi
+    JOIN order_item_notes oin
+        USING (order_id, product_id);
+    
+    -- EXERCISE: use sql_invoicing and extract date, client, amount and payment type from payments table
+    USE sql_invoicing;
+    SHOW tables;
+    DESCRIBE payments;
+    DESCRIBE clients;
+    DESCRIBE payment_methods;
+
+    SELECT
+        p.date,
+        c.name,
+        p.amount,
+        pm.name
+    FROM payments p
+    JOIN clients c
+        USING (client_id)
+    JOIN payment_methods pm
+        ON p.payment_method = pm.payment_method_id;
+
+
+-- NATURAL JOIN
+    -- we don't specify the attributes on which to join
+    -- database will look and join based on common attributes
+
+    -- from sql_store join orders and customers
+    USE sql_store;
+    SHOW tables;
+    SELECT *
+    FROM orders
+    NATURAL JOIN customers;
+
+-- CROSS JOIN
+    -- it will join every row from first column to every column in the second column
+    SELECT
+        c.first_name AS customer,
+        p.name AS product
+    FROM customers c
+    CROSS JOIN products p -- explicit syntax
+    ORDER BY customer;
+
+    -- implicit syntax through from clause
+    SELECT
+        c.first_name AS customer,
+        p.name AS product
+    FROM customers c, products p -- inplicit syntax
+    ORDER BY customer;
+
+    -- EXERCISE: do cross join between shippers and products
+        DESCRIBE shippers;
+        DESCRIBE products;
+
+        -- using the implicit syntax
+        SELECT
+            p.name AS product,
+            s.name AS shipper
+        FROM products p, shippers s;
+
+        -- and using the explicit syntax
+        SELECT
+            p.name AS product,
+            s.name AS shipper
+        FROM products p
+        CROSS JOIN shippers s;
+
+-- UNION
+    -- join rows from multiple table
+    system cls;
+    SELECT *
+    FROM orders;
+
+    -- if order is placed this year then active otherwise archieve
+    SELECT
+        order_id,
+        order_date,
+        'Active' AS status -- here string literal is used for indicating status
+    FROM orders
+    WHERE order_date >= "2019-01-01"
+        
+        UNION
+
+    SELECT
+        order_id,
+        order_date,
+        'Archieved' AS status -- here string literal is used for indicating status
+    FROM orders
+    WHERE order_date < "2019-01-01";
+
+    -- name of customers and shippers in single column 
+    -- (although not real world use case but for understanding)
+    SELECT
+        first_name
+    FROM customers
+
+        UNION
+    
+    SELECT
+        name
+    FROM shippers;
+
+    -- NOTE: while taking union columns returned by all sets must be same otherwise MySQL will yell
+    -- NOTE: whatever the name of first set will be used as column name for all combined set after union
+
+    -- from customers table extract customer id, first name, points and
+    -- differenciate type based on points
+        -- < 2000 => Bronze
+        -- Between 2000 & 3000 => Silver
+        -- > 3000 => Gold
+
+    system cls;
+    SELECT
+        customer_id,
+        first_name,
+        points,
+        'Bronze' AS type
+    FROM customers
+    WHERE points < 2000
+
+    UNION
+
+    SELECT
+        customer_id,
+        first_name,
+        points,
+        'Silver' AS type
+    FROM customers
+    WHERE points BETWEEN 2000 AND 3000
+
+    UNION
+
+    SELECT
+        customer_id,
+        first_name,
+        points,
+        'Gold' AS type
+    FROM customers
+    WHERE points > 3000
+    
+    ORDER BY first_name; -- if sorting is needed then it must be in last union query
+
+
+
